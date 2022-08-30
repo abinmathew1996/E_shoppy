b3ef4145694db554af2adb196f0fc88b9f385bfb
abinmathew1996 <abin.m.n007@gmail.com>
2022-08-30 14:51:31 +0530

    full backend

--- eshoppy_backend/models/category.js
+++ eshoppy_backend/models/category.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 
 const categorySchema = mongoose.Schema({
-
     name: {
         type: String,
         required: true,
@@ -12,7 +11,15 @@ const categorySchema = mongoose.Schema({
     color: { 
         type: String,
     }
-
 })
+
+
+categorySchema.virtual('id').get(function () {
+    return this._id.toHexString();
+});
+
+categorySchema.set('toJSON', {
+    virtuals: true,
+});
 
 exports.Category = mongoose.model('Category', categorySchema);
--- eshoppy_backend/routes/categories.js
+++ eshoppy_backend/routes/categories.js
@@ -2,7 +2,6 @@ const {Category} = require('../models/category');
 const express = require('express');
 const router = express.Router();
 
-//reading productlist
 router.get(`/`, async (req, res) =>{
     const categoryList = await Category.find();
 
@@ -12,17 +11,16 @@ router.get(`/`, async (req, res) =>{
     res.status(200).send(categoryList);
 })
 
-//reading a single productlist
-
 router.get('/:id', async(req,res)=>{
     const category = await Category.findById(req.params.id);
 
     if(!category) {
         res.status(500).json({message: 'The category with the given ID was not found.'})
-    }   
+    } 
     res.status(200).send(category);
 })
-// adding a product
+
+
 
 router.post('/', async (req,res)=>{
     let category = new Category({
@@ -38,7 +36,23 @@ router.post('/', async (req,res)=>{
     res.send(category);
 })
 
-// deleteing a product
+
+router.put('/:id',async (req, res)=> {
+    const category = await Category.findByIdAndUpdate(
+        req.params.id,
+        {
+            name: req.body.name,
+            icon: req.body.icon || category.icon,
+            color: req.body.color,
+        },
+        { new: true}
+    )
+
+    if(!category)
+    return res.status(400).send('the category cannot be created!')
+
+    res.send(category);
+})
 
 router.delete('/:id', (req, res)=>{
     Category.findByIdAndRemove(req.params.id).then(category =>{
