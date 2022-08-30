18634df4b732c23f44b1b002c1762c2a09253add
abinmathew1996 <abin.m.n007@gmail.com>
2022-08-30 11:15:40 +0530

    get products and get product

--- eshoppy_backend/routes/categories.js
+++ eshoppy_backend/routes/categories.js
@@ -2,14 +2,27 @@ const {Category} = require('../models/category');
 const express = require('express');
 const router = express.Router();
 
+//reading productlist
 router.get(`/`, async (req, res) =>{
     const categoryList = await Category.find();
 
     if(!categoryList) {
         res.status(500).json({success: false})
     } 
-    res.send(categoryList);
+    res.status(200).send(categoryList);
 })
+
+//reading a single productlist
+
+router.get('/:id', async(req,res)=>{
+    const category = await Category.findById(req.params.id);
+
+    if(!category) {
+        res.status(500).json({message: 'The category with the given ID was not found.'})
+    }   
+    res.status(200).send(category);
+})
+// adding a product
 
 router.post('/', async (req,res)=>{
     let category = new Category({
@@ -24,6 +37,8 @@ router.post('/', async (req,res)=>{
 
     res.send(category);
 })
+
+// deleteing a product
 
 router.delete('/:id', (req, res)=>{
     Category.findByIdAndRemove(req.params.id).then(category =>{
